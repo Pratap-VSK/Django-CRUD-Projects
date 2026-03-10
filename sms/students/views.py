539796bbac65d3e7652acf29067297e48adbb076
Pritam <itspratap255@gmail.com>
2026-03-10 22:29:52 +0530

    project initialized

---
+++ sms/students/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, redirect
+
+from .models import Student
+
+# Create your views here.
+
+def student_home(request):
+
+    students_data = Student.objects.all()
+
+
+    data = {
+        "students_data": students_data
+    }
+
+    return render(request, "students/student_home.html", data)
+
+
+def add_student(request):
+
+
+    if request.method == "POST":
+        student_name = request.POST.get("input_name")
+        student_email = request.POST.get("input_email")
+        student_phone_number = request.POST.get("input_phone_number")
+
+        Student.objects.create(
+            name = student_name,
+            email = student_email,
+            phone_number = student_phone_number
+        )
+
+        return redirect("student_home")
+
+    return render(request, "students/add_student.html")
+
+def delete_student(request, student_id):
+
+    my_student = Student.objects.get(id = student_id)
+
+    my_student.delete()
+
+    return redirect("student_home")
+
+def update_student(request, student_id):
+
+    student = Student.objects.get(id = student_id)
+
+    if request.method == "POST":
+        student.name = request.POST.get("name")
+        student.email = request.POST.get("email")
+        student.phone_number = request.POST.get("phone")
+        
+        student.save()
+
+        return redirect("student_home")
+
+    parameters = {
+        "student": student
+    }
+
+    return render(request, "students/update_student.html", parameters)
+
